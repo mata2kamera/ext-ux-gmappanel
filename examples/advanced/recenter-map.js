6ef4e5b16f3b5ba8d8b400a680d4d9b88cf7cbda
justarock <justarock@030912a1-9d55-0410-a717-135fea14408a>
2009-03-13 15:04:23 +0000

    example for re-centering via geocoding and zooming a map in different ways.

---
+++ examples/advanced/recenter-map.js
@@ -0,0 +1,76 @@
+/**
+ * @author Shea
+ */
+
+Ext.onReady(function(){
+
+    var mapwin;
+    var button = Ext.get('show-btn');
+
+    button.on('click', function(){
+        // create the window on the first click and reuse on subsequent clicks
+        if(!mapwin){
+
+            mapwin = new Ext.Window({
+                layout: 'fit',
+                title: 'GMap Window',
+                closeAction: 'hide',
+                width:400,
+                height:400,
+                x: 40,
+                y: 60,
+                items: {
+                    xtype: 'gmappanel',
+                    zoomLevel: 14,
+                    gmapType: 'map',
+                    id: 'my_map',
+                    mapConfOpts: ['enableScrollWheelZoom','enableDoubleClickZoom','enableDragging'],
+                    mapControls: ['GSmallMapControl','GMapTypeControl','NonExistantControl'],
+                    setCenter: {
+                        geoCodeAddr: '4 Yawkey Way, Boston, MA, 02215-3409, USA',
+                        marker: {title: 'Fenway Park'}
+                    },
+					buttons: [{
+						text: 'Museum of Fine Arts',
+						handler: function(){
+							Ext.getCmp('my_map').geoCodeLookup('465 Huntington Avenue, Boston, MA, 02215-5597, USA', null, false, true, null);
+						}
+					},{
+						text: 'Fenway Park',
+						handler: function(){
+							Ext.getCmp('my_map').geoCodeLookup('4 Yawkey Way, Boston, MA, 02215-3409, USA', null, false, true, null);
+						}
+					},{
+						text: 'Zoom Fenway Park',
+						handler: function(){
+							// this way will apply the zoom level to every map move
+							Ext.getCmp('my_map').zoomLevel = 19;
+							Ext.getCmp('my_map').geoCodeLookup('4 Yawkey Way, Boston, MA, 02215-3409, USA', null, false, true, null);
+							// or you can set it just once
+							// Ext.getCmp('my_map').getMap().setZoom(19);
+						}
+					},{
+						text: '+',
+						minWidth: 30,
+						handler: function(){
+							var m = Ext.getCmp('my_map').getMap();
+							m.setZoom(m.getZoom()+1);
+						}
+					},{
+						text: '-',
+						minWidth: 30,
+						handler: function(){
+							var m = Ext.getCmp('my_map').getMap();
+							m.setZoom(m.getZoom()-1);
+						}
+					}]
+                }
+            });
+            
+        }
+        
+        mapwin.show();
+        
+    });
+    
+ });
